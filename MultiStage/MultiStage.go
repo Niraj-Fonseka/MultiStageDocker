a20139d29c5277849d4dd91c092bc51235791fbd
Niraj-Fonseka <fonseka.live@utexas.edu>
2019-02-05 00:30:34 -0600

    Adding the single stage and multi stage

---
+++ MultiStage/MultiStage.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	randomdata "github.com/Pallinder/go-randomdata"
+)
+
+func main() {
+	fmt.Println("Running the TestApp")
+	fmt.Println(randomdata.SillyName())
+}
---
+++ SingleStage/go.mod
@@ -0,0 +1,3 @@
+module MultiStageDocker/SingleStage
+
+require github.com/Pallinder/go-randomdata v1.1.0
